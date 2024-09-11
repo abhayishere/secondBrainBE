0c5711251049a5ce679e4be10f327939814a029e
Abhay Yadav <62054505+abhayishere@users.noreply.github.com>
2024-09-11 09:29:50 +0530

    Added secured firebase credential for render

--- firebase.go
+++ firebase.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"log"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 )
 
@@ -14,8 +17,20 @@ var client *firestore.Client
 
 func initFirebase() {
 	ctx := context.Background()
-	opt := option.WithCredentialsFile("/Users/abhayishere/Downloads/SecondBrain/backend/secondbrain-6ba99-firebase-adminsdk-221bw-3bbdda7e16.json")
-
+	err1 := godotenv.Load()
+	if err1 != nil {
+		log.Fatalf("Error loading .env file: %v", err1)
+	}
+	credsbase64 := os.Getenv("FIREBASE_CREDENTIALS")
+	if credsbase64 == "" {
+		log.Fatalf("FIREBASE_CREDENTIALS environment variable not set")
+	}
+	credsJSON, err2 := base64.StdEncoding.DecodeString(credsbase64)
+	if err2 != nil {
+		log.Fatalf("Error decoding Firebase credentials: %v", err2)
+	}
+	creds := []byte(credsJSON)
+	opt := option.WithCredentialsJSON(creds)
 	var err error
 	app, err = firebase.NewApp(ctx, nil, opt)
 	if err != nil {
